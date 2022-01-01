250eabfe8214c74d0c783b1a8c2c05e2915d327a
ronakmehtav <ronakmehtav@gmail.com>
2022-01-01 14:03:13 +0530

    movement added

--- snakeGame.js
+++ snakeGame.js
@@ -2,6 +2,35 @@ const ROWS = 60;
 const COLS = 100;
 const PIXEL = 10;
 const canvas = document.getElementById("canvas");
+let currentDirection = "down";
+const DirectionalObj = {
+  up: [-1, 0],
+  down: [1, 0],
+  left: [0, -1],
+  right: [0, 1],
+};
+
+window.onkeydown = (key) => {
+  console.log(key.key);
+  if (key.key === "w") {
+    console.log(key);
+    currentDirection = "up";
+  } else if (key.key === "d") {
+    console.log(key);
+    currentDirection = "right";
+  } else if (key.key === "a") {
+    console.log(key);
+    currentDirection = "left";
+  } else if (key.key === "s") {
+    console.log(key);
+    currentDirection = "down";
+  }
+};
+
+function addArray(direction, elm) {
+  const val = DirectionalObj[direction];
+  return [val[0] + elm[0], val[1] + elm[1]];
+}
 
 function initializeCanvas() {
   for (let row = 0; row < ROWS; row++) {
@@ -62,23 +91,30 @@ function renderSnake(newSnakePosition) {
 }
 
 function run() {
-  newSnakePosition = snakePosition.slice();
-  newSnakePosition.shift(0);
-  let elm = newSnakePosition.at(-1);
-  elm = [elm[0], elm[1] + 1];
-  if (isSnakeGoingOutOfCanvas(elm)) {
-    return;
-  }
-  newSnakePosition.push(elm);
+  createNextSnakePosition();
   renderSnake(newSnakePosition);
   setSnakePosition(newSnakePosition);
+}
+
+function createNextSnakePosition() {
+  newSnakePosition = snakePosition.slice();
+  let elm = newSnakePosition.at(-1);
+  elm = addArray(currentDirection, elm);
+  if (isSnakeGoingOutOfCanvas(elm)) {
+    gameOver();
+    return;
+  }
+  newSnakePosition.shift(0);
+  newSnakePosition.push(elm);
 }
 
 function isSnakeGoingOutOfCanvas(elm) {
   return elm[1] >= COLS || elm[0] >= ROWS || elm[1] < 0 || elm[0] < 0;
 }
 
-function game() {
-  setInterval(run, 100);
+const currentGame = setInterval(run, 150);
+
+function gameOver() {
+  clearInterval(currentGame);
+  console.log("executing termination");
 }
-game();
